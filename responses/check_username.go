3579f90564e5e24be544e66e2f54256d510a5ab0
kiru <me@kiru.xyz>
2019-10-28 23:41:30 +0000

    Update check_username.go

--- responses/check_username.go
+++ responses/check_username.go
@@ -3,7 +3,7 @@ package responses
 type CheckUsername struct {
 	Response
 	Username  string `json:"username,omitempty"`
-	Available string `json:"available,omitempty"`
+	Available bool `json:"available,omitempty"`
 	Error     string `json:"error,omitempty"`
 	ErrorType string `json:"error_type,omitempty"`
 }
